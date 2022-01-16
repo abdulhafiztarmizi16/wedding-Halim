2cd94f1f670c003781f8910682456d301e04af98
abdulhafiztarmizi16 <shuishinekun16>
2022-01-16 10:59:38 +0700

    firstUpload

---
+++ connect.php
@@ -0,0 +1,10 @@
+<?php
+$servername ="localhost";
+$username = "root";
+$password = "";
+$database = "test";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+?>
